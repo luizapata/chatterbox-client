031554925dd8120f5a2f4a50a4c801c7d7d850b6
luizapata <lufeza95@gmail.com>
2016-07-02 15:58:49 -0700

    working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,102 +1,88 @@
 // YOUR CODE HERE:
 
-var app = Backbone.Model.extend({
-  initialize: function(username, roomName){
-    this.set({username: username, roomName: roomName})
-    if (roomName) {
-      this.set("roomName", roomName)
-    }
-    
-  },
-  defaults {
-    roomName: "lobby "
-  },
-  send: function(){
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'POST',
-      data: JSON.stringify(this.get("message", "username")),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (data) {
-        console.error('chatterbox: Failed to send message');
-      }
-    });
-  },
-  fetch: function(){
-    $.ajax({
-      url: "https://api.parse.com/1/classes/chatterbox",
-      type: 'GET',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-      console.log('chatterbox: Message recived');
-      },
-      error: function (data) {
-      console.error('chatterbox: Failed to fetch message');
-      }
-    });
-  },
-  clearMessages: function(){
-    $("#chats").empty();
-  },
-  addMessage: function(message){
-    var messa = $("<div class=message >" + message.text + "</div>");
-    var user = $("<div class=username >" + message.username + "</div>");
-    user.append(messa)
-     $(".username").on('click', function(){
-        app.addFriend();
-      });
-    $('#chats').append(user)
-  },
-  addRoom: function(roomName){
-    $('#roomSelect').append("<option>" + roomName + "</option>")
-  },
-  addFriend: function(){     
-  console.log('hi')
-  },
-  handleSubmit:function(){
-    
-  }
-});
-
-
-var messageVIew = Backbone.View.extend({
-  initialize: function(){
-
-  }
-})
-
-
-
-
-
 $(document).ready(function(){
-  $('#send .submit').on('submit', function(){
-    app.handleSubmit()
+  
+  var No = function(message){
+    this.user = 'La NO';
+    this.text = message;
+    this.roomname = 'lobby';
+  };
+
+  $('#send ').on('submit', function(e){
+    e.preventDefault()
+    var m = $("#message").val();
+    var messages = new No(m)
+    // app.addMessage(messages);
+    // console.log(messages)
+    app.send(messages)
+    $("#message").val("");
   });
+
+  var app = {
+    init: function(){
+    },
+    send: function(text){
+      $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'POST',
+        data: JSON.stringify(text),
+        contentType: 'application/json',
+        success: function (data) {
+          console.log('chatterbox: Message sent');
+          app.addMessage(text)
+        },
+        error: function (data) {
+          console.error('chatterbox: Failed to send message');
+        }
+      });
+    },
+    fetch: function(){
+      $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'GET',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function (data) {
+          console.log('chatterbox: Message recived');
+          _.each(data.results, function(obj){
+            app.addMessage(obj)
+          })
+        },
+        error: function (data) {
+          console.error('chatterbox: Failed to fetch message');
+        }
+      });
+    },
+    clearMessages: function(){
+      $("#chats").empty();
+    },
+    addMessage: function(message){
+      var messa = $("<p>" + message.text + "</p>");
+      var user = $("<h3 class=username >" + message.user + "</h3>");
+      var div = $("<div class=messageBox></div><br/>");
+      user.append(messa)
+      div.append(user)
+       $(".username").on('click', function(){
+          app.addFriend();
+        });
+      $('#chats').prepend(div)
+    },
+    addRoom: function(roomName){
+      $('#roomSelect').append("<option>" + roomName + "</option>")
+    },
+    addFriend: function(){     
+    console.log('hi')
+    },
+    handleSubmit:function(){
+    }
+  };
+
+  // setInterval(function(){
+    app.fetch();
+    
+  // },3000)
+  
 })
     
-
-
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
